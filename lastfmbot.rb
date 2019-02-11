d674ac7cde9d943f465a9e17535407d694c17e93
Alisson Ariel S. Machado <aamachado@inf.ufsm.br>
2019-02-10 23:34:34 -0200

    Stage 1 completed
    the design is done, is still needing implement the database and choose the server.

---
+++ lastfmbot.rb
@@ -0,0 +1,36 @@
+require 'telegram/bot'
+require 'unicode/emoji'
+require_relative 'lastfmcode'
+#the secret tokens for this bot
+require_relative 'tokens'
+
+puts TOKEN
+
+  Telegram::Bot::Client.run(TOKEN, logger: Logger.new($stderr)) do |bot|
+  bot.logger.info('funcionando')
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_photo(chat_id: message.chat.id, photo: 'https://extra.globo.com/incoming/14451917-103-193/w640h360-PROP/1cadela-assalto-campanha.jpg' ,caption: "Oi, bem vindo, para me usar digite seu usuário com /, ex.: /usuario")
+    else
+      if(message.text.include? '/')
+        teste = (message.text.partition("/"))
+        texto = nil
+        messagemm = teste[2]
+        song = lastFMbo(messagemm)
+        texto =" \u{1F60D} #{message.chat.first_name} "
+        if(song['isListening'])
+          texto = texto + listening(song)
+        else texto = texto + waslistening(song)
+        end
+        if(song['image']['link']!=nil)
+          bot.api.send_photo(chat_id: message.chat.id, photo: "#{song['image']['link']}", caption: "#{texto}")
+        else bot.api.send_message(chat_id: message.chat.id, text: "#{texto}")
+        end
+      else
+        bot.api.send_photo(chat_id: message.chat.id, photo: 'https://pbs.twimg.com/profile_images/617110894162616320/2x38ptfg_400x400.png' ,caption: "Para de tentar me prejudicar, digite seu usuário com /, ex.: /usuario")
+    end
+  end
+end
+end
+
---
+++ lastfmcode.rb
@@ -0,0 +1,54 @@
+require 'scrobbler2'
+
+def datasong(song, texto)
+  return texto = "#{texto}\n \u{1F3B6} #{song['name']}\n \u{1F465} #{song['artist']}\n \u{1F4BF} #{song['album']}"
+end
+  
+def listening(song)
+  texto ="is listening for the #{playcount(song)} \u{2716}time to:"
+  datasong(song, texto)
+end
+
+def waslistening(song)
+  texto ="was listening for the #{playcount(song)} \u{2716}time to:"
+  datasong(song, texto)
+end
+
+def playcount(song)
+  return case song['playcount']
+  when 1
+    "1st"
+  when 2
+    "2nd"
+  when 3
+    "3rd"
+  else
+    "#{song['playcount']}th"
+  end
+end
+
+
+def lastFMbo (username)
+  Scrobbler2::Base.api_key = "2ab8d4422f6e0b17392839c1ea5d32a4"
+  Scrobbler2::Base.api_secret = "810a76e4f9802d31b42dc66dee9b2ac6"
+
+    
+  user = Scrobbler2::User.new(username)
+  controlador = 0
+  b = Hash.new
+  a = user.recent_tracks["track"][0]
+  b['isListening'] = a.include? '@attr'
+  b['name'] = a['name']
+  b['artist'] = a['artist']['#text']
+  track = Scrobbler2::Track.new(b['artist'], b['name'], username)
+  if(track.info.class != NilClass)
+    b['playcount'] = track.info['userplaycount'].to_i+1
+  end
+  b['album'] = a['album']['#text']
+  b['image'] = Hash.new
+  if(!a['image'][3]['#text'].to_s.empty?)
+    b['image']['link'] = a['image'][3]['#text']
+    b['image']['size'] = a['image'][3]['size']
+  end
+  return b
+end
